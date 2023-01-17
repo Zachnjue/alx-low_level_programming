b6b4c09d7fdcda8463260f3ba8d8467090bfc378
Zachnjue <njuezachary@gmail.com>
2023-01-17 09:37:40 -0800

    edited 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -40,8 +40,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0; i < lname, i++)
-		o_dog->name[i] = name[i];
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
 
 	p_dog->age = age;
